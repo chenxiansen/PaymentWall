ad18c6c6d78b4fba803ae4cd6f0d7ae4c6e666b5
chenxiansen <15951982354@163.com>
2022-03-24 19:01:14 +0800

    update

--- test/index.php
+++ test/index.php
@@ -2,18 +2,18 @@
 require_once('../lib/paymentwall.php');
 Paymentwall_Config::getInstance()->set(array(
     'api_type' => Paymentwall_Config::API_GOODS,
-    'public_key' => '',
-    'private_key' => ''
+    'public_key' => 'e23b0408f3e308f01ae36d807a8cd25f',
+    'private_key' => '671446f9456d6535337d7ce3e2747b0c'
 ));
 
 $widget = new Paymentwall_Widget(
-    '2323123', // uid
+    '555155', // uid
     'p1', // widget
     array(
-    	'shop_id'=>1,
-    	'shop_name'=>'钻石',
-    	'shop_amount'=>1
-    ), // Product parts, leave empty for Widget API
+        'shop_id'=>1,
+        'shop_name'=>'钻石',
+        'shop_amount'=>2
+    ),
     array(
         'email' => '15951982354@163.com', 
         'history[registration_date]' => 'registered_date_of_user',
@@ -21,7 +21,5 @@ $widget = new Paymentwall_Widget(
         'addtional_param_name' => 'addtional_param_value'
     )
 );
-
 echo $widget->getHtmlCode();
-
 ?>
